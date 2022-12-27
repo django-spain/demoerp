2d336f9cf01ade20d8768117138ac30e55e830af
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon-2.local>
2022-12-27 03:33:59 +0100

    58 Herencia de Modelo

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
      # MyApps
     'bases',
+    'inv',
 ]
 
 MIDDLEWARE = [
--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    usuario_creacion = models.CharField(max_length=50)
+    usuario_modificacion = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
